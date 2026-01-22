9582c1214ac04b99b57a7043d849689400dbba89
Fehlingerist <ksztnnnn@gmail.com>
2026-01-22 19:23:15 +0100

    Add VSCode configuration files for building and debugging; update build script to include Tokenizer path

--- build.ps1
+++ build.ps1
@@ -1,2 +1,1 @@
-g++ -std=c++26 src/main.cpp src/lexer/lexer.cpp -I"C:/dev/C_C++/StdToolset/" -I"src" -D_DEBUG -o "build/math_eval.exe" -g
-#g++ -std=c++26 -O3 src/main.cpp src/lexer/lexer.cpp -I"C:/dev/C_C++/StdToolset/" -I"src" -o "build/math_eval.exe"
+g++ -std=c++26 src/main.cpp src/lexer/lexer.cpp -I"C:/dev/C_C++/StdToolset/" -I"C:/dev/C_C++/Tokenizer/" -I"src" -D_DEBUG -o "build/math_eval.exe" -g
